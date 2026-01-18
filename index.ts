029a9c92436aeff88303cfdd6f8a66b568027279
Lawrence Moore <thelawrencemoore@gmail.com>
2026-01-18 13:28:24 -0800

    Downgrade `pdf-parse` to v1.1.4 and integrate text splitting in PDF processing script.

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import {RecursiveCharacterTextSplitter} from '@langchain/textsplitters';
 
 const testPdfPath = "papers/VectorDB/1-s2.0-S1389041724000093-main.pdf"
 
 async function main() {
     try {
         const loader = new PDFLoader(testPdfPath)
+        const splitter = new RecursiveCharacterTextSplitter({chunkSize: 500, chunkOverlap: 50});
 
         const docs = await loader.load()
-        console.log(docs[0]);
+        const output = await splitter.splitDocuments(docs);
+        console.log(output);
     } catch (e) {
         console.error(e)
     }
